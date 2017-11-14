8cdb4e64f0249fb8a9c034833e2fc6e1b369e7c0
Diederik de Groot <dkgroot@talon.nl>
2017-11-14 16:11:32 +0100

    Merge ata186 and ata188 Update ringtones Add Desktops/wallpapers

--- tftpboot/ringtones/mkringlist.sh
+++ tftpboot/ringtones/mkringlist.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 outfile=ringlist.xml
 echo -e "<CiscoIPPhoneRingList>" >$outfile
-for filename in *.pcm;do
-	basename=`basename ${filename} .pcm`
-	echo -e "\t<Ring>" >>$outfile
-	echo -e "\t\t<DisplayName>${basename}</DisplayName>" >>$outfile
-	echo -e "\t\t<FileName>ringtones/${filename}</DisplayName>" >>$outfile
-	echo -e "\t</Ring>" >>$outfile
-done
-for filename in *.raw;do
-	basename=`basename ${filename} .raw`
-	echo -e "\t<Ring>" >>$outfile
-	echo -e "\t\t<DisplayName>${basename}</DisplayName>" >>$outfile
-	echo -e "\t\t<FileName>ringtones/${filename}</DisplayName>" >>$outfile
-	echo -e "\t</Ring>" >>$outfile
-done
+if [ ! -z "`ls *.pcm 2>/dev/null`" ]; then
+	for filename in *.pcm;do
+		basename=`basename ${filename} .pcm`
+		echo -e "\t<Ring>" >>$outfile
+		echo -e "\t\t<DisplayName>${basename}</DisplayName>" >>$outfile
+		echo -e "\t\t<FileName>${filename}</FileName>" >>$outfile
+		echo -e "\t</Ring>" >>$outfile
+	done
+fi
+if [ ! -z "`ls *.raw 2>/dev/null`" ]; then
+	for filename in *.raw;do
+		basename=`basename ${filename} .raw`
+		echo -e "\t<Ring>" >>$outfile
+		echo -e "\t\t<DisplayName>${basename}</DisplayName>" >>$outfile
+		echo -e "\t\t<FileName>${filename}</FileName>" >>$outfile
+		echo -e "\t</Ring>" >>$outfile
+	done
+fi
 echo -e "</CiscoIPPhoneRingList>" >>$outfile
 
