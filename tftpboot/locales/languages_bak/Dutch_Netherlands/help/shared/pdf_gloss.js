7dc4fd9d5c4fc50d18375908be8b7537046f09c3
Diederik de Groot <dkgroot@talon.nl>
2018-03-05 16:50:55 +0100

    Remove languages_bak directory

--- tftpboot/locales/languages_bak/Dutch_Netherlands/help/shared/pdf_gloss.js
+++
@@ -1,9 +0,0 @@
-if(parent.pdfUrl != "")
-{
-theMenu.addChild(-1, "Document", parent.pdf , parent.pdfUrl, parent.pdf , parent.pdfTarget);
-}
-
-if(parent.glossaryUrl != "")
-{
-theMenu.addChild(-1, "Document", parent.glossary , parent.glossaryUrl, parent.glossary, parent.glossaryTarget);
-}
