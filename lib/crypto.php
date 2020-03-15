d4562e3683abb40cd8e9bd248b1ad34d3dd1f673
Marcello Ceschia <marcello.ceschia@gmail.com>
2020-03-15 22:29:09 +0100

    add crypto lib

---
+++ lib/crypto.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace SCCP\Crypto;
+
+class HashAlgo extends Enum {
+    private const SHA1 = 0;
+    private const SHA256 = 1;
+}
+
+class Certificate {
+    function __construct(string $file) {
+		$this->file = $file;
+    }
+    
+    public function hashAsBase64(int $hashAlg)  : string  {
+        return "";
+    }
+}
+
+class Crypto {
+
+    public static function certificateHashAsBase64(string $certificateFile, int $hashAlg)  : string  {
+        return new Certificate($certificateFile)
+                .hashAsBase64($hashAlg);
+    }
+}
+
+class Signer {
+}
