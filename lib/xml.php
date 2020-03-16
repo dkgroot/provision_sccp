4d0eab9693c1c9ccd10c9f5e69a937e9dc4d96a7
Diederik de Groot <ddegroot@talon.nl>
2020-03-16 08:27:00 +0100

    Added php-xml to README Added initial lib/xml.php Renamed srv/tftp/tftp_provisioner.php -> srv/tftp/tftp_provisioner
    Signed-off-by: Diederik de Groot <ddegroot@talon.nl>

---
+++ lib/xml.php
@@ -0,0 +1,54 @@
+<?php
+include_once("config.php");
+
+class XML {
+	private $config;
+	private $data;
+	private $values;
+	private $index;
+	private $parser;
+	function __construct($config) {
+		$this->config = $config;
+		$this->parser = xml_parser_create();
+		//xml_error_string 
+	}
+	function __destruct() {
+		xml_parser_free($this->parser);
+	}
+	function readStr($content) {
+		xml_parse_into_struct($this->parser , $content , $this->values, $this->index);
+	}
+	function readFile($filename) {
+		$content = file_get_contents($filename);
+		$this->read($content);
+	}
+	function write() {
+	}
+	function addAttr($elem, $attr) {
+	}
+	function removeAttr($elem, $attr) {
+	}
+	function updateAttr($elem, $attr) {
+	}
+	function add($parent) {
+	}
+	function remove($parent) {
+	}
+	function update($parent) {
+	}
+}
+
+// Testing
+if(defined('STDIN') ) {
+	$xml = new XML($config);
+	$test_cases = Array(
+	);
+	foreach($test_cases as $test) {
+		try {
+			$xml->readStr("<device></device>");
+		} catch (Exception $e) {
+		}
+	}
+	unset($resolver);
+}
+?>
