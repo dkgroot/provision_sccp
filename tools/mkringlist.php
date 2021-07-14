250f86b2603c3987c35dc7690b0efed5fd6dc42e
steve-lad <72376554+steve-lad@users.noreply.github.com>
2021-07-14 10:22:09 +0200

    Convert mkringlist to php and remove from ringtones
    Remove mkringlist.sh from tftpboot and move to tools.
    Convert to php
    Remove executables from tftpboot

---
+++ tools/mkringlist.php
@@ -0,0 +1,34 @@
+<?php
+
+//Usage: php mkringlist.php;
+
+function createCats($currDirPos){
+
+    $xml = '';
+
+    if(is_dir($currDirPos)){
+        // Remove unix ./..
+        foreach (array_diff(scandir($currDirPos),array('.','..')) as $subDir) {
+            //remove link information
+            $subDir = explode(' ', $subDir);
+						$displayName = explode('.',$subDir[0]);
+            $fileName = str_replace('../tftpboot/','',$currDirPos) .'/'. $subDir[0];
+            if (in_array($displayName[1], array('pcm','raw'))) {
+    						$xml .= "<Ring><DisplayName>{$displayName[0]}</DisplayName><FileName>{$fileName}</FileName></Ring>";
+            }
+        }
+    }
+    return $xml;
+}
+
+function saveXml($xml, $filename) {
+    $xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'.'<CiscoIPPhoneRingList>' . $xml . '</CiscoIPPhoneRingList>';
+    $dom = new \DOMDocument("1.0");
+    $dom->preserveWhiteSpace = false;
+    $dom->formatOutput = true;
+    $dom->loadXML($xml);
+    $dom->save($filename);
+}
+
+saveXml(createCats('../tftpboot/ringtones'),'../tftpboot/ringtones/ringlist.xml');
+?>
